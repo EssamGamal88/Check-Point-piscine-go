f3f8542c25cf2b5c0b999504d27a7e56a0c0f4e8
Essam Gamal <104252912+EssamGamal88@users.noreply.github.com>
2025-11-26 16:18:33 +0200

    Simplify prime checking in FindPrevPrime function
    Refactor FindPrevPrime function to check for prime directly from input number.

--- findprevprime.go
+++ findprevprime.go
@@ -6,26 +6,17 @@ func FindPrevPrime(nb int) int {
 	if nb < 2 {
 		return 0
 	}
-
-	for n := nb; n >= 2; n-- {
-		isPrime := true
-
-		for i := 2; i*i <= n; i++ {
-			if n%i == 0 {
-				isPrime = false
-				break
-			}
-		}
-
-		if isPrime {
-			return n
+	for i := 2; i*i <= nb; i++ {
+		if nb%i == 0 {
+			return isprime
 		}
 	}
-
 	return 0
 }
 
 func main() {
 	fmt.Println(FindPrevPrime(5))
 	fmt.Println(FindPrevPrime(4))
+	fmt.Println(FindPrevPrime(1))
+
 }
